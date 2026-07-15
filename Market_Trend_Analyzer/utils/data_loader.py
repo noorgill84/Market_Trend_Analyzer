8e702c663282788b3cd4d34abde98b2553242e8b
website <tejnoors79@gmail.com>
2026-07-15 12:17:29 +0530

    Create data_loader.py

---
+++ Market_Trend_Analyzer/utils/data_loader.py
@@ -0,0 +1,17 @@
+"""
+=========================================================
+Market Trend Analyzer
+Data Loader Module
+=========================================================
+Loads datasets and trained ML model.
+
+Author : Your Name
+=========================================================
+"""
+
+import streamlit as st
+import pandas as pd
+import joblib
+from pathlib import Path
+
+# ------------------------------------------------
